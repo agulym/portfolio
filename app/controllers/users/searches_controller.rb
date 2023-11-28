d10cc2a32017cfb2c977694990afbac20d03683d
agulym <91873703+agulym@users.noreply.github.com>
2023-11-28 02:00:05 +0000

    5

---
+++ app/controllers/users/searches_controller.rb
@@ -0,0 +1,11 @@
+class Users::SearchesController < ApplicationController
+    def search
+      @electronics_or_user = params[:option]
+      if @electronics_or_user == '1'
+        @electronics = Electronics.search(params[:search], @electronics_or_user)
+      else
+        @users = User.search(params[:search], @electronics_or_user)
+      end
+      render action: :search
+    end
+  end
